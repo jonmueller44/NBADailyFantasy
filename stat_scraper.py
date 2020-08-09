f2857d26daa2dc69d04459b5845537c328e62b81
jonmueller44 <55213303+jonmueller44@users.noreply.github.com>
2020-08-08 21:11:55 -0700

    add typing to functions

--- stat_scraper.py
+++ stat_scraper.py
@@ -1,17 +1,23 @@
 from bs4 import BeautifulSoup
 from string import ascii_lowercase
 import urllib.request
+from typing import List, Tuple
+
+PlayerInfo = Tuple[str, str]  # player_id, name
+PlayerList = List[PlayerInfo]
 
 BASKETBALL_REFERENCE_BASE_URL = "https://www.basketball-reference.com/"
 BASKETBALL_REFERENCE_PLAYERS_BASE_URL = BASKETBALL_REFERENCE_BASE_URL + "players/"
 
-def get_active_players_by_letter(letter: str):
+def get_active_players_by_letter(letter: str) -> PlayerList:
     assert len(letter) == 1
     assert letter.isalpha()
 
     url = BASKETBALL_REFERENCE_PLAYERS_BASE_URL + letter
     html = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(html, 'html.parser')
+
+    player_list = []
     
     table = soup.find('table')
     active_players_strong = table.find_all('strong')
@@ -19,12 +25,16 @@ def get_active_players_by_letter(letter: str):
         player = strong.a.string
         player_id = strong.a.get("href")
         player_id = player_id[11:player_id.index('.')]
-        print(str(player) + " " + str(player_id))
+        player_list.append((player_id, player))
+
+    return player_list
 
 
-def get_active_players():
+def get_active_players() -> PlayerList:
+    player_list = []
     for letter in ascii_lowercase:
-        get_active_players_by_letter(letter)
+        player_list.extend(get_active_players_by_letter(letter))
 
+    return player_list
 
-get_active_players_by_letter('a')
+print(get_active_players())
